89116e0918b58c8ab6b54558dddb56cc7a4fd34b
Jakub Smékal <jakub.smekal@gmail.com>
2023-10-01 12:45:38 -0700

    rebase

--- app/pages/2_diary.py
+++ app/pages/2_diary.py
@@ -65,3 +65,40 @@ else:
         daily_outputs = pickle.load(file)
     for k, v in daily_outputs.items():
         st.write(f"**{k.capitalize().replace('_', ' ')}:** {v}")
+
+# Set the title of the app
+st.title('Daily Diary/Journal')
+
+# Check if there's a list of entries already stored
+if 'entries' not in st.session_state:
+    st.session_state.entries = []
+
+# Section to write a new diary/journal entry
+st.subheader('Write a New Entry')
+new_entry = st.text_area('Your Entry...')
+
+if st.button('Submit Entry'):
+    # Attach a timestamp to the entry
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    st.session_state.entries.append((timestamp, new_entry))
+    
+    # Store the updated list of entries in a pickle file
+    with open('diary_entries.pickle', 'wb') as file:
+        pickle.dump(st.session_state.entries, file)
+
+    st.success('Entry saved!')
+
+# Section to display all the entries for the specific date
+current_date = datetime.now().strftime('%Y-%m-%d')
+st.subheader(f'{current_date} Entries')
+
+# Filter the entries to show only those from the current date
+today_entries = [(time, entry) for timestamp, entry in st.session_state.entries if timestamp.startswith(current_date)]
+
+if today_entries:
+    for time, entry in today_entries:
+        st.markdown(f"**{time.split(' ')[1]}**")  # Only displaying the time part of the timestamp
+        st.write(entry)
+        st.write('---')  # adds a separator line for better readability
+else:
+    st.write(f"No entries for {current_date} yet!")
