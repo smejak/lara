bbf66b35a7116f723459dcbbe44ca457cfb4667c
Jakub Smékal <jakub.smekal@gmail.com>
2023-10-14 15:04:43 -0700

    WIP: added eval_res

--- app/pages/5_daily_LLM.py
+++ app/pages/5_daily_LLM.py
@@ -1,6 +1,47 @@
 import streamlit as st
 from datetime import datetime
 import pickle
+import openai
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+
+def eval_res(question, user_response):
+    prompt_ = """
+            You are LARA, an advanced digital assistant designed specifically to help people with dementia. Individuals with dementia often face challenges in recalling memories, recognizing familiar faces, and performing daily tasks. As the condition progresses, they might also find it challenging to navigate their surroundings, remember their medication schedules, or even recollect personal history and family details.
+            Determine whether the user's answer to the following question makes sense, return OK or REPEAT depending on whether you think the user should have included more information.
+            
+            Example 1:
+
+            Question: List down the activities you engaged in today
+            User Response: Hello
+            Verdict: REPEAT
+
+            Example 2:
+            Question: What were the highlight of your day? Mention any interactions with family, friends, or caregivers. What are the other important information I should be aware of?
+            User Response: I went shopping today to Trader Joe's, this was at 11am. Then I met my daughter Claire for lunch at 1pm. Now I am in my house cooking pasta for dinner. I am looking forward to watching TV later.
+            Verdict: OK
+            
+            Question:
+            {question}
+
+            User Response:
+            {user_response}
+
+            Be very pleasant, always address the patient by their name, have an empathetic tone.
+            
+            ###
+            
+            Output the Question response:
+
+            """
+            
+    template = PromptTemplate(template=prompt_, input_variables=["question" ,"user_response"])
+    
+    llm = OpenAI(model_name="gpt-4", temperature=0.5, max_tokens=300)
+    prompt = template.format(question=question, user_response=user_response)
+    res = llm(prompt)
+
+    return res
 
 # Set the title of the app
 st.title('Daily Diary')
